8f35ffcaecdc717c6ae22b28361b55acc3908a6c
Eduardo Rodrigues da Silva <silva.rodriguesedu@icloud.com>
2021-09-11 20:51:28 -0300

    Project multitenancy-schema-spring created

---
+++ src/main/java/br/com/exemplos/eduardo/multitenancyschemaspring/MultitenancySchemaSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.exemplos.eduardo.multitenancyschemaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenancySchemaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultitenancySchemaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/exemplos/eduardo/multitenancyschemaspring/MultitenancySchemaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.exemplos.eduardo.multitenancyschemaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultitenancySchemaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
